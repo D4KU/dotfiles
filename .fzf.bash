89c2f3b5c9e5dacf173cf79253023042e6522265
D4KU <d4k@tuta.io>
2020-11-09 22:38:49 +0100

    fzf.bash: extended fasd support

--- .fzf.bash
+++ .fzf.bash
@@ -165,11 +165,29 @@ ftpane() {
 }
 
 ######################## Fasd ########################
-# fasd & fzf change directory
 # open best matched file using `fasd` if given argument,
 # filter output of `fasd` using `fzf` else
 fv() {
     [ $# -gt 0 ] && fasd -f -e ${EDITOR} "$*" && return
     local file
     file="$(fasd -Rfl "$1" | fzf -1 -0 --no-sort +m)" && vi "${file}" || return 1
+}
+
+# open best matched dir using `fasd` if given argument,
+# filter output of `fasd` using `fzf` else
+fz() {
+    [ $# -gt 0 ] && fasd_cd -d "$*" && return
+    local dir
+    dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort)" && cd "${dir}" || return 1
+}
+
+# cd into directory of file chosen via fasd
+fvz() {
+  local file
+  if [ $# -gt 0 ]; then
+    file=$(fasd -f "$*")
+  else
+    file="$(fasd -Rfl "$1" | fzf -1 -0 --no-sort)"
+  fi
+  cd "$(dirname "$file")"
 }
